768a75ff61917680e7067447c20e64371022f962
Jacob George <kooldrjay060@gmail.com>
2024-04-20 22:51:30 +0100

    added error handling to form via radix themes

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,20 +1,56 @@
 'use client';
-import React from 'react'
-import { TextField, Button } from '@radix-ui/themes'
+import React, { useState } from 'react'
+import { TextField, Button, Callout } from '@radix-ui/themes'
 import SimpleMDE from "react-simplemde-editor";
+import {useForm, Controller } from 'react-hook-form';
 import "easymde/dist/easymde.min.css";
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+
+interface IssueFormInterface{
+    title : string
+    description : string
+}
 
 const NewIssue = () => {
-  return (
-    <div className='max-w-xl space-y-3'>
-        <TextField.Root placeholder='title'>
-        </TextField.Root>
+    const router = useRouter();
+    const { register,control, handleSubmit } = useForm<IssueFormInterface>();
 
-        <SimpleMDE placeholder="Description" />
+    const [error, setError] = useState('')
+    
+    return (
+        <div className='max-w-xl '>
+            {
+                error && (
+                <Callout.Root color='red' className='mb-4'>
+                    <Callout.Text>{error}</Callout.Text>
+                </Callout.Root>
+            )}
+            
+            <form 
+                onSubmit={handleSubmit( async (data) => {
+                    try {
+                        await axios.post('/api/issues',data)
+                        router.push('/issues')
+                    } catch (error) {
+                        setError('An unexpected event occurred')
+                    }
+                } )} 
+                className='space-y-3'
+            >
+                <TextField.Root placeholder='title' {...register('title')}></TextField.Root>
 
-        <Button>Create New Issue</Button>
-    </div>
-  )
+                <Controller 
+                    name='description'
+                    control={control}
+                    render={ ({field}) => <SimpleMDE placeholder="Description" {...field} /> }
+                />
+                
+                <Button>Create New Issue</Button>
+            </form>
+        </div>
+       
+    )
 }
 
 export default NewIssue
